e91a2dba87e50b122e02149dde72fb22dcdabe29
Jonathan-Gonzalez0 <jonathan.gonzalez57@upr.edu>
2025-07-26 10:16:05 -0400

    modified app.py for better compatibility with face

--- app.py
+++ app.py
@@ -8,8 +8,17 @@ Created on Wed Jul 23 19:59:52 2025
 from ImageClassificationClass import ImageRegressor
 import gradio as gr
 
-Dog_Model = ImageRegressor()
 
+Dog_Model = ImageRegressor()
 Dog_Model.LoadModel("Dog_Classifier_Model")
 
-gr.Interface(fn = Dog_Model.ImagePredict, inputs = gr.Image(type = "pil"), outputs = "label").launch()
+def predict_fn(image):
+    print("Function called!")
+    return Dog_Model.ImagePredict(image)
+
+if __name__ == "__main__":
+    gr.Interface(
+        fn=predict_fn,
+        inputs=gr.Image(type="pil"),
+        outputs="label"
+    ).launch()
