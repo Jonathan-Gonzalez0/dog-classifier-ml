f742f5d335ef09c6a16d1e7512ca448d71689153
Jonathan-Gonzalez0 <jonathan.gonzalez57@upr.edu>
2025-07-27 11:23:32 -0400

    Debugging

--- app.py
+++ app.py
@@ -5,19 +5,9 @@ Created on Wed Jul 23 19:59:52 2025
 @author: Jonathan gonzalez
 """
 
-from ImageClassificationClass import ImageRegressor
 import gradio as gr
 
-Dog_Model = ImageRegressor()
-Dog_Model.LoadModel("Dog_Classifier_Model")
+def hello(image):
+    return "Hello!"
 
-def predict_fn(image):
-    print("Function called!")
-    return Dog_Model.ImagePredict(image)
-
-if __name__ == "__main__":
-    gr.Interface(
-        fn=predict_fn,
-        inputs=gr.Image(type="pil"),
-        outputs="label"
-    ).launch(share=True)
+gr.Interface(fn=hello, inputs=gr.Image(type="pil"), outputs="label").launch(debug=True)
